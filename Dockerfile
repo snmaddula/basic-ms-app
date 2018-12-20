756a1ead78e68f75a4b66a35a3cbe29672726bdc
snmaddula <snmaddula@outlook.com>
2018-12-20 16:24:09 +0530

    Converting to war packaging

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM openjdk:8u181-jdk-alpine
-
-VOLUME /tmp
-
-ADD target/basic-ms-app-1.0.jar app.jar
-
-RUN touch /app.jar
-
-CMD ["/bin/sh", "-c",  ". /etc/profile && java -Djava.security.egd=file:/dev/./urandom -jar app.jar"]
--- src/main/java/com/pochub/ms/BasicMsApp.java
+++ src/main/java/com/pochub/ms/BasicMsApp.java
@@ -2,10 +2,17 @@ package com.pochub.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BasicMsApp {
+public class BasicMsApp extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BasicMsApp.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BasicMsApp.class, args);
 	}
